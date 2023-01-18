4312277cb50aa5922091b9ff78582978dda5f529
Robertsnemeth <104597057+Robertsnemeth@users.noreply.github.com>
2023-01-18 13:16:39 -0800

    another update for success message

--- client/src/components/styled_components/SuccessMessage.jsx
+++ client/src/components/styled_components/SuccessMessage.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { motion } from "framer-motion";
 
 const SuccessMessage = () => {
   return (
-    <h1 className="bg-green-500 p-2 text-white rounded">Your Message Was successfully Sent</h1>
+    <motion.div
+    initial={{ opacity: 0}}
+    whileInView={{ opacity: 1 }}
+    transition={{ duration: 1 }}
+    transitionEnd>
+        <h1 className="bg-green-500 p-2 text-white rounded">Your Message Was successfully Sent</h1>
+    </motion.div>
   )
 }
 
